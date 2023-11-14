71cacd4f5413046dcfcae387bdb5e4852f48874b
Loris Salvador <loris.salvador@student.hepl.be>
2023-11-15 00:59:29 +0100

    e

---
+++ 2.4 InjectionDependancesManuel/InjectionDependancesManuel/src/main/java/be/hepl/injectiondependancesmanuel/InjectionDependancesManuelApplication.java
@@ -0,0 +1,13 @@
+package be.hepl.injectiondependancesmanuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectionDependancesManuelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InjectionDependancesManuelApplication.class, args);
+    }
+
+}
---
+++ 2.4 InjectionDependancesManuel/InjectionDependancesManuel/src/test/java/be/hepl/injectiondependancesmanuel/InjectionDependancesManuelApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hepl.injectiondependancesmanuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjectionDependancesManuelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
