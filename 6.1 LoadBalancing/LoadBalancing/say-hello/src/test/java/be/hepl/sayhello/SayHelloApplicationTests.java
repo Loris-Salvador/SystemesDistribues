fc2ef290ff2a8fbc957868db2e98d6a783c45b21
Loris Salvador <loris.salvador@student.hepl.be>
2024-01-15 03:19:41 +0100

    6.1

---
+++ 6.1 LoadBalancing/LoadBalancing/say-hello/src/test/java/be/hepl/sayhello/SayHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hepl.sayhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
